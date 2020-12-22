ded8ee84316cf9f794dfabe61f7d687601364fa0
Dominic Trang <dominic_t@hotmail.co.nz>
2020-12-22 19:26:45 +1300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/**
+ * @require Libraries
+ */
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            Hello world!
+        </div>
+    );
+}
+
+export default App;
