40664f4f4538948b69988f185444098690c66451
Dominic Trang <dominic_t@hotmail.co.nz>
2020-12-22 23:58:25 +1300

    fix(utils): Flip export to fix test export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
 export * from './money.util';
-export * from './test.util';
 export * from './localstorage.util';
+
+// -- Keep this at the bottom as this is a jsx file
+export * from './test.util';
